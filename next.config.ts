138b807b641eb7f8bb5fcd0c8e4a4d5eed6268b8
Chetna Kumar <chetnakumar93@gmail.com>
2026-05-21 21:58:17 +0530

    added

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",   // ⭐ IMPORTANT for GitHub Pages
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
+        protocol: "https",
+        hostname: "images.pexels.com",
       },
     ],
+
+    unoptimized: true, // ⭐ REQUIRED for static export
   },
-    allowedDevOrigins: ['192.168.20.149'],
+
+  allowedDevOrigins: ["192.168.20.149"],
 };
 
 export default nextConfig;
